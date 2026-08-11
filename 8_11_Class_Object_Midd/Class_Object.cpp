17f8187888791ee2890ad8e54d43e63edd89befb
hhuiy1107-bite <hhuiy1107@gmail.com>
2026-08-11 23:27:55 +0800

    拷贝构造函数学习

---
+++ 8_11_Class_Object_Midd/Class_Object.cpp
@@ -0,0 +1,38 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+
+//#include <iostream>
+//using namespace std;
+//
+//class Data
+//{
+//public:
+//
+//	void Init(int year, int month, int day)
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}
+//
+//	void print()
+//	{
+//		cout << this->_year << "/" << this->_month << "/" << this->_day << endl;
+//	}
+//
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//
+//int main()
+//{
+//	Data d1;
+//	d1.Init(2026, 8, 10);
+//	d1.print(&d1);
+//
+//	Data d2;
+//	d2.Init(2026, 8, 11);
+//	d2.print(&d2);
+//	return 0;
+//}
